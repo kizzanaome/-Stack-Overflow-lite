32c36e19d6c607b80fa7de18bed51682b3b74081
[kizzanaome] <[naomekizzan@gmail.com]>
2018-08-20 07:51:45 -0700

    Document views file [#159846498]

--- app/questions/views.py
+++ app/questions/views.py
@@ -3,13 +3,14 @@ from flask_restful import Api, abort, reqparse, Resource
 from .models import question_db, Question, Replytoquestion,answers
 
 class View_questions(Resource):
-
     def get(self):
+        """Method for getting questions"""
         if not question_db:
             return make_response(jsonify({"message":"No questions posted yet"}),200)
         return make_response(jsonify({"questions": question_db}),200)
 
     def post(self):
+        """Method for posting a question"""
         parser =reqparse.RequestParser()
         parser.add_argument("title")
         parser.add_argument("description")
@@ -33,7 +34,7 @@ class Singlequestion(Resource):
             if (question["qstn_id"] == qstn_id):
                 a_qustn = question
                 return make_response(jsonify({"question":a_qustn}), 200)
-        return make_response(jsonify({"massage":"Question not found"}),404)
+        return make_response(jsonify({"message":"Question not found"}),404)
 
 class Answerquestion(Resource):
     def post(self, qstn_id):
