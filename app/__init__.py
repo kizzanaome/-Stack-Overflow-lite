f8684f2de1226ff155b44ce7e130cf83810c894d
[kizzanaome] <[naomekizzan@gmail.com]>
2018-08-21 16:04:28 -0700

    Add doc_strings to file[Delivers #159847176]

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,18 @@ from instance.config import app_config
 
 
 def create_app(config_name):
+    """create_app function loads the correct configuration 
+       from the config.py given a configuration name
+    """
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    """
+    Registering blueprints
+
+    """
+
 
     from .questions import questions as questions_blueprints
     app.register_blueprint(questions_blueprints)
